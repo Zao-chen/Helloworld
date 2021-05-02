a4b21d94775b503fd9b2cfe7082a9898405843f3
Zao-chen <77674075+Zao-chen@users.noreply.github.com>
2021-05-02 17:14:58 +0800

    Add files via upload

---
+++ HelloWorld - 2021.5.2/src/main/java/me/zaochen/helloworld/HelloWorld.java
@@ -0,0 +1,29 @@
+package me.zaochen.helloworld;
+
+import me.zaochen.helloworld.commands.*;
+import me.zaochen.helloworld.events.fish;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HelloWorld extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("[HelloWorld] 插件已加载");
+        getCommand("helloworld").setExecutor(new maincommand());
+        getCommand("helloworld").setTabCompleter(new maincommand());
+        getCommand("add").setExecutor(new add());
+        getCommand("gonggao").setExecutor(new gonggao());
+        getCommand("config").setExecutor(new config());
+        getCommand("change").setExecutor(new change());
+        getCommand("health").setExecutor(new health());
+        getServer().getPluginManager().registerEvents(new fish(),this);
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("[HelloWorld] 插件已卸载");
+    }
+}
