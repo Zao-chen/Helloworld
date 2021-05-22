6a0e4b44bd91351300f9512ceb44f219498c0e5d
Zao-chen <77674075+Zao-chen@users.noreply.github.com>
2021-05-22 22:19:40 +0800

    Add files via upload

---
+++ HelloWorld - 2021.5.9/src/main/java/me/zaochen/helloworld/Papi.java
@@ -0,0 +1,100 @@
+package me.zaochen.helloworld;
+
+import org.bukkit.OfflinePlayer;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.plugin.Plugin;
+
+import java.util.List;
+
+/**
+ * This class will automatically register as a placeholder expansion
+ * when a jar including this class is added to the directory
+ * {@code /plugins/PlaceholderAPI/expansions} on your server.
+ * <br>
+ * <br>If you create such a class inside your own plugin, you have to
+ * register it manually in your plugins {@code onEnable()} by using
+ * {@code new YourExpansionClass().register();}
+ */
+public class Papi extends PlaceholderExpansion {
+
+    /**
+     * This method should always return true unless we
+     * have a dependency we need to make sure is on the server
+     * for our placeholders to work!
+     *
+     * @return always true since we do not have any dependencies.
+     */
+    @Override
+    public boolean canRegister(){
+        return true;
+    }
+
+    /**
+     * The name of the person who created this expansion should go here.
+     *
+     * @return The name of the author as a String.
+     */
+    @Override
+    public String getAuthor(){
+        return "Zao_chen";
+    }
+
+    /**
+     * The placeholder identifier should go here.
+     * <br>This is what tells PlaceholderAPI to call our onRequest
+     * method to obtain a value if a placeholder starts with our
+     * identifier.
+     * <br>The identifier has to be lowercase and can't contain _ or %
+     *
+     * @return The identifier in {@code %<identifier>_<value>%} as String.
+     */
+    @Override
+    public String getIdentifier(){
+        return "helloworld";
+    }
+//%helloworld.xxx%
+    /**
+     * This is the version of this expansion.
+     * <br>You don't have to use numbers, since it is set as a String.
+     *
+     * @return The version as a String.
+     */
+    @Override
+    public String getVersion(){
+        return "1.0.0";
+    }
+
+    /**
+     * This is the method called when a placeholder with our identifier
+     * is found and needs a value.
+     * <br>We specify the value identifier in this method.
+     * <br>Since version 2.9.1 can you use OfflinePlayers in your requests.
+     *
+     * @param  player
+     *         A {@link org.bukkit.OfflinePlayer OfflinePlayer}.
+     * @param  identifier
+     *         A String containing the identifier/value.
+     *
+     * @return Possibly-null String of the requested identifier.
+     */
+    @Override
+    public String onRequest(OfflinePlayer player, String identifier){
+
+        // %example_placeholder1%
+        if(identifier.equals("see")){
+            return "Helloworld is working now";
+        }
+
+        // %example_placeholder2%
+
+        if(identifier.equals("placeholder2")){
+            Plugin config = me.zaochen.helloworld.HelloWorld.getProvidingPlugin(me.zaochen.helloworld.HelloWorld.class);
+            String head = "§e[Helloworld]§r ";
+            return config.getConfig().getString("string");
+        }
+
+        // We return null if an invalid placeholder (f.e. %example_placeholder3%)
+        // was provided
+        return null;
+    }
+}
